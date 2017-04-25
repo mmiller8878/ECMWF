0145dec808ada519aaf7dba80c778feebaac3490
mmiller8878 <matt.miller@agrimetrics.co.uk>
2017-04-25 12:29:33 +0100

    added function stubs

--- etl functions.py
+++ etl functions.py
@@ -1,5 +1,10 @@
+
+
 def ETL_based_upon_forecast_type():
     '''looks at the name of the forecast and redirects to appropriate ETL based upon this'''
+
+def create_string_for_location():
+    '''the forecast gives the centre of a grid as a lat/long. We need each corner to generate a URI'''
 
 def ETL_to_table():
     '''takes all the forecasts produced at a specific timepoint and ETLs them into the same table'''
